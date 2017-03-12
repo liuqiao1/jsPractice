c0b7b229bb1a1527eb55813219305070d9a071d4
liuqiao1 <3504735@qq.com>
2017-03-12 10:39:09 +0800

    3.12

--- scripts/chapter6.js
+++ scripts/chapter6.js
@@ -130,6 +130,21 @@ function createTest(){
     Person.prototype.sayName = function(){
         alert(this.name);
     }
+    /**
+     * 更简单的原型语法 
+     * 相当于创建了一个新对象，让prototype指向它
+     * 这时新对象的constructor属性 不再指向Person  而是指向Object
+     * 但是，如果需要的话，可以强行指向
+     * 
+     * constructor:person 
+     */
+    /*Person.prototype = {
+        name:"jack",
+        age:22,
+        sayName:function(){
+            alert(this.name);
+        }
+    }*/
 
     var person3 = new Person();
     //alert(person3.name);//jack 通过空构造函数访问原型对象
@@ -148,9 +163,9 @@ function createTest(){
      * 如何判断某个属性是实例属性，还是原型属性
      * object.hasOwnProperty("Attr");
      */
-    alert(person3.hasOwnProperty("name"));//true
-    alert(person4.hasOwnProperty("name"));//flase
-    alert(person4.hasOwnProperty("noAttr"));//false 
+    //alert(person3.hasOwnProperty("name"));//true
+    //alert(person4.hasOwnProperty("name"));//flase
+    //alert(person4.hasOwnProperty("noAttr"));//false 
 
     /**
      * 返回false有两种情况：1.实例没有有这个属性，但原型有 2.实例和原型都没有这个属性 如何辨别?
@@ -159,10 +174,66 @@ function createTest(){
      * 接下来再用hasOwnProperty就可以知道 是实例的还是原型的
      */
     if(name in person4){
-        alert(person4.hasOwnProperty("name"));
+        //alert(person4.hasOwnProperty("name"));
     }
     else{
-        alert("no this Attribute");
+        //alert("no this Attribute");
+    }
+
+    /**
+     * 原型的动态性
+     * 一个对象 的原型对象已经写好了，如果我创建了一个实例，再去修改原型对象，会怎样
+     */
+    var person5 = new Person();
+    Person.prototype.name = "Changed";
+    //alert(person5.name);//输出Changed 。先在实例中找name,没找到，去找原型中的，找到了，尽管被改了也不影响
+    /**
+     * 一个对象 的原型对象已经写好了，如果我创建了一个实例，再去【重写】！原型对象，会怎样
+     */
+    var person6 = new Person();//创建好实例 prototype 指向原来的原型对象
+    Person.prototype={//实例对象重写，但是与 person6 没有任何关联，除非在person6生成之前重写实例
+        name:"Jack",
+        age:44,
+        test: function(){
+            alert("Dynamic!");
+        }
+    }
+    alert(person6.age);//没有任何东西输出
+
+
+}
+
+/**
+ * 原型 的缺点
+ * 1.不支持传递构造参数  2.不希望共享的属性也会被共享
+ * 一般情况下，每个实例的属性值不同，但是使用的方法相同
+ * 那么属性定义采用构造方法，方法定义使用原型
+ * 组合二者，互补
+ */
+function combine(){
+   //alert("combine");
+    // Book.prototype={
+    //     open:function(){
+    //         alert("open:"+this.name);
+    //     }
+    // }
+     var book = new Book("Math","Martin");
+     alert(book.name + book.author);
+     book.open();
+
+}
+
+function Book(name,author){
+    this.name = name;
+    this.author = author;
+    //如果想把 属性 和原型方法封装到一起呢？
+    //创建原型open方法的代码只会执行一次
+    //第一次，open不存在，制作好，下一次创建实例的时候可以直接用源性对象里的open
+    if(typeof this.open != "function"){
+        //alert("no");
+        Book.prototype.open=function(){
+            alert("open:"+this.name);
+     }
     }
 }
 
@@ -186,11 +257,152 @@ function sayName(){
 function Person(){
 }
 
+////////////////////////////////////////继承////////////////////////////////
 
+function Animal(){
+    this.name = "defaultAnimalName";
+    this.colors = ["black","white"];
+    if(typeof this.call != "function"){
+        Animal.prototype.call = function(){
+             alert("animal call"+this.name);
+         }
+    }
+}
+/** 
+ * 借用构造函数  组合模式（传自己的构造参数）
+ */
+function Dog(name){  
+    Animal.call(this);//------------第二次创建Animal--------------------
+    this.name = name;//这一句放在前，没用；放在后有用.为什么呢？
+    //难道是 放在前面的话，会被覆盖掉？
+    //放在后面有点追加的意思
+}
+
+// function Dog(name){
+//     this.name = name;
+// }
+
+function Cat(name){
+    Animal.call(this);
+    this.name = name;
+}
+
+function testInherite(){
+    //alert("testInherite");
+    var animal = new Animal();//------------第一次创建Animal--------------
+    //alert(animal.name);
+    /**
+     * 通过下面这句继承
+     */
+    Dog.prototype = animal;
+    var dog = new Dog("dog");
+   
+    //dog.prototype = animal;
+    //alert(dog.name);
+   // dog.call();
+
+    //dog 原型加一个自己的方法 
+    Dog.prototype.run = function(){
+        alert("run!");
+    }  
+
+    //dog.run();
+    //animal.run();//其实是加在了animal上
+
+    //重写原型的方法
+    Dog.prototype.call=function(){
+        alert("call again!");
+    }
+    //dog.call();
+    //animal.call();//覆盖了原来的call方法
+
+    /**
+     * 使用 原型链 实现继承的缺点 和 使用原型创建实例 的缺点类似
+     * 1.不希望共享的也被共享
+     * 2.不能传递构造参数
+     */
+    Cat.prototype = animal;
+    var cat = new Cat("cat");
+   
+    //alert(dog.colors);
+    dog.colors.push("red");
+    alert(cat.colors); 
+
+}
+/**
+ * 所有函数的默认原型 都是object实例
+ */
+/**
+ * 测试借用构造函数 组合继承(子对象构造函数追加自己的属性)
+ */
+
+function borrowConstrutor(){
+    //Calls a method of an object, substituting another object for the current object.
+    Dog.prototype = new Animal();
+    var myDog = new Dog("myDog");
+    //alert(myDog.colors);
+    //myDog.call();
+
+    Cat.prototype = new Animal();
+    var myCat = new Cat("myCat");
+    //alert(myCat.name);
+
+    myDog.colors.push("red");
+    //alert(myDog.colors);
+    //alert(myCat.colors);//这样就算狗的颜色修改了，也不会影响猫  为什么
+
+   // alert(myDog.call == myCat.call);//true
+    //猫和狗共享一个原型 ？
+   // alert(Dog.prototype == Cat.prototype);//false  说明不是同一个
+
+    /**
+     * 原型式继承
+     */
+   //var yx = getChild(Animal); 错！
+   var yx = getChild(new Animal());//得到的是一个实例
+   //alert(yx.colors);   //输出了颜色
+   var child = parasite(new Animal);
+   child.sayHi();//输出Hello I'm a Child 
+}
+/**
+ * 原型式继承 只给temp写了一个最简单的构造函数，在想得到一个与object类似的对象（没有过多个性）的对象时
+ * 可以用原型式继承
+ */
+function getChild(object){
+    function temp(){};
+    temp.prototype = object;
+    //return temp;
+    return new temp();
+}
+/**
+ * 寄生式继承 在原型式继承的基础上，再封装点特性
+ */
+
+function parasite(object){
+    var child = getChild(object);
+    child.sayHi = function(){
+        alert("Hello I'm a Child");
+    }
+    return child;
+}
+
+/**
+ * 寄生组合式继承
+ * subType:子对象
+ * superType:父对象
+ * ------------不理解---------------------
+ */
+function inheritPrototype(subType, superType){
+    var prototype = object(superType.prototype); //创建对象
+    prototype.constructor = subType; //增强对象
+    subType.prototype = prototype; //指定对象
+}
 
 //window.onload = def2;
 //window.onload = dataAttr;
 //window.onload = visisAttr;
-window.onload = createTest;
-
+//window.onload = createTest;
+//window.onload = combine;
+//window.onload = testInherite;
+window.onload = borrowConstrutor;
 
---
+++ scripts/chapter7.js
@@ -0,0 +1,188 @@
+/**
+ * Chapter 7 函数表达式
+ * 递归 闭包 模仿块级作用域 私有变量
+ */
+
+/**
+ * 递归
+ * 求阶乘
+ */
+function factorial(num){
+    if(num<=1){
+        return 1;
+    }
+    else{
+        //return num * factorial(num-1);
+
+        return num*arguments.callee(num-1);//推荐方式
+    }
+}
+/**
+ * 严格模式下 递归
+ */
+var strictFactorial = (function f(num){
+    if (num <= 1){
+    return 1;
+    } else {
+    return num * f(num-1);
+    }
+});
+
+/**
+ * 闭包
+ */
+function createCpmpare(property){
+
+    return function(object1,object2){
+        //内部匿名函数可以访问到外部变量 property
+        var value1 = object1[property];
+        var value2 = object2[property];
+
+        if(value1 <= value2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+}
+/**
+ * 闭包陷阱
+ */
+function createFunc(){
+    var result = new Array();
+    
+    for(var i=0;i<10;i++){
+        // result[i] = function(){
+        //     return i;
+        // }
+
+        result[i] = function(num){
+            return num;
+        }
+    }
+
+    return result;
+}
+
+/**
+ * 在全局函数中， this 等于 window，而当函数被作为某个对象的方法调用时， this 等于那个对象。
+ * 匿名函数的执行环境具有全局性，因此其 this 对象通常指向 window①。
+ * 但有时候由于编写闭包的方式不同，这一点可能不会那么明显。
+ * 
+ * 前面曾经提到过，每个函数在被调用时都会自动取得两个特殊变量： this 和 arguments。
+ * 内部函数在搜索这两个变量时，只会搜索到其活动对象为止，因此永远不可能直接访问外部函数中的这两个变量
+ */
+var name = "windowName";
+function testThis(){
+ 
+    var bean = {
+        name:"thisName",
+        sayHi:function(){
+            var hiFunc = function(){
+                alert(this.name);
+            }
+            return hiFunc;
+        }
+    }
+    alert(bean.name);
+    bean.sayHi()();
+}
+
+function Main(){
+    //alert("Main");
+    //测试求阶乘
+    //alert(factorial(4));//24
+    //接下来我用另一个指针指向函数对象
+    var anotherFactorial = factorial;
+    factorial = null;//断开原来的
+    //alert(anotherFactorial(4));//无输出
+    //因为factorial函数内部 自己调用自己的时候，还是返回的factorial(num-1)
+    //但是这是factorial已经和函数本身没有关联了
+
+    //return num*arguments.callee(num1-1);//推荐方式
+    //-----------------------
+    var compare = createCpmpare("name");
+    //alert(compare);
+    var result = compare({name:"lily"},{name:"michael"});
+    //alert(result);//  1
+    //------------------------------------
+    var funcs = createFunc();
+    alert(funcs[0](0));
+    alert(funcs[1](1));
+    alert(funcs[9](9));
+}
+/**
+ * 私有变量 私有方法
+ */
+function Student()
+{
+    /**
+     * new Student时  
+     * var obj  = new object();
+     * 然后当前执行环境转到obj 上 ，此时this 指obj
+     * 
+     * obj.getName = .....
+     * 
+     * return obj;
+     */
+    var name = "liu";
+    function exam(){
+        alert("exam!");
+    }
+
+    this.getName = function(){
+        return name;//闭包访问外部的name
+        //return this.name 就错了
+    }
+}
+/**
+ * 通过构造函数的方式来创建私有变量访问方法
+ * 缺点：每次生成一个新实例，都会产生一套相同的方法
+ * 如果可以实现共享方法可以解决这个问题
+ * 使用原型
+ */
+function Person(name){
+    //name 是一个私有变量 在构造函数外部只能用下面两个方法访问
+    this.getName = function(){
+        return name;
+    };
+    this.setName = function (value) {
+        name = value;
+    };
+}
+
+
+function testPrivate(){
+    //alert("testPrivate");
+     var stu = new Student();
+    // this.age = 11;
+    // alert(stu.name);//undefined
+    // alert(stu.age);//undefined
+    // alert(Student.name);//Student
+     alert(stu.getName());
+
+    var person = new Person("Nicholas");
+    alert(person.getName()); //"Nicholas"
+    person.setName("Greg");
+    alert(person.getName()); //"Greg
+    
+    /**
+     * java 私有变量
+     * Class A(){
+     *      private name;
+     *      
+     *      public getA();setA();
+     * }
+     * A a = new A();
+     * a.name = "liuqiao";//这样做会报错哦，因为name是私有变量
+     * //如果开放了set方法：
+     * A.setA("liuqiao");这样是对的
+     * 
+     */
+}
+
+//window.onload = Main;
+//window.onload = testThis;
+window.onload = testPrivate;
